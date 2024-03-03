a557ea61af1b87b70b773e98c396ac76fc715201
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-03-03 23:54:33 +0530

    Create grades.py

---
+++ grades.py
@@ -0,0 +1,12 @@
+#program to print the grade of the student 
+n= float(input("enter the percentage of the student :"))
+if n>90:
+    print("a grade ")
+elif (n>80):
+    print("grade b")
+elif (n>70):
+    print("c grade")
+elif n>=60:
+    print("d grade")
+else:
+    print("grade e ")
